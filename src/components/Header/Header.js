ccd4eedd219c09381772bf4ff3d2dca12cd3b478
Didar2255 <didar.a2255@gmail.com>
2021-09-25 17:16:34 +0600

    use hover in manu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../../Image/icon.png'
+import'./Header.css'
 
 const Header = () => {
   return (
@@ -9,8 +10,8 @@ const Header = () => {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 manu-bar">
             <li className="nav-item">
               <a className="nav-link active text-white" aria-current="page" href="/#">Home</a>
             </li>
